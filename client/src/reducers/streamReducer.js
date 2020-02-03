6b9d67f219048430d584ea40bee2f12509b2af79
parth-gulati <gulati.parth98@gmail.com>
2020-02-04 00:52:32 +0530

    Add files via upload

---
+++ client/src/reducers/streamReducer.js
@@ -0,0 +1,30 @@
+//Array Based approach
+
+import _ from "lodash";
+
+import {
+  EDIT_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  CREATE_STREAM,
+  DELETE_STREAM
+} from "../actions/types";
+
+const streamReducer = (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      return _.omit(state, action.payload);
+    default:
+      return state;
+  }
+};
+
+export default streamReducer;
